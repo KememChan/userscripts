bf92e2a8efd4db41eb268bff9834d111cf304880
DayatXD <enderboykrenz@gmail.com>
2023-01-05 19:55:00 +0800

    fixed javstore and refactor the code a bit

--- msin+/msin+.user.js
+++ msin+/msin+.user.js
@@ -11,75 +11,96 @@
 // ==/UserScript==
 
 // Remove fucking Title
-$(".movie_title").remove()
+$(".movie_title").remove();
 
 $("span.movie_fileName").remove();
-$('body').after(`<script>function copyToClipboard(element) {var $temp = $("<input>");$("body").append($temp);$temp.val($(element).text()).select();document.execCommand("copy");$temp.remove();}</script>`);
-$('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">')
+$("body").after(
+  `<script>function copyToClipboard(element) {var $temp = $("<input>");$("body").append($temp);$temp.val($(element).text()).select();document.execCommand("copy");$temp.remove();}</script>`
+);
+$("head").append(
+  '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">'
+);
+
+let birth = $("span.mv_barth");
+if (birth.length) {
+  var birthYear = birth.text().split("-")[0];
+}
+
+$(".movie_create").before(
+  `<div class='sukebei'><a class='loading'>Sukebei Loading...</a></div>`
+);
+var codeArray = [];
 var x = 0;
 var row = $("div.movie_info");
-
-let birth = $("span.mv_barth")
-if (birth.length) { var birthYear = birth.text().split("-")[0] }
-
-var codeArray = []
-
-$(row).each(function() {
-// TO DO REFACTOR THIS TO NOT USE EACH FUNC
+$(row).each(function () {
+  // TO DO REFACTOR THIS TO NOT USE EACH FUNC
   if (birthYear != null) {
-    let videoCreated = $(this).find("div.movie_create")
-    let videoCreatedYear = videoCreated.text().split("-")[0]
-    let sumHerAge = videoCreatedYear - birthYear
-    videoCreated.children("a").append(`(${sumHerAge} Years Old)`)
+    let videoCreated = $(this).find("div.movie_create");
+    let videoCreatedYear = videoCreated.text().split("-")[0];
+    let sumHerAge = videoCreatedYear - birthYear;
+    videoCreated.children("a").append(`(${sumHerAge} Years Old)`);
   }
 
-  let kurung = $(this).find('span.movie_pn').parent().parent()
+  let kurung = $(this).find("span.movie_pn").parent().parent();
   if (kurung.length) {
-    kurung.html(kurung.html().replace(/[\(\)]+/g, ''))
+    kurung.html(kurung.html().replace(/[\(\)]+/g, ""));
   }
   var code = $(this).find(".movie_pn");
-  code.html(code.html().replace("_", "-"));
-  code.html(code.html().toUpperCase())
-  var code = $(this).find(".movie_pn");
-  // let match = /^(carib|caribpr|1pon|paco|FC2-PPV)+-(\d.+)/gi;
-  let match = /^(carib|caribpr|1pon|paco)+-(\d.+)/gi;
-  let hasil = match.exec(code.html())
-  match.test(code.html()) // if i didnt add this the fucking if test will not working bruh...
-  var codeUrl
-  if (match.test(code.html())) {
-    codeUrl = hasil[2]
-    codeArray.push(hasil[2])
-  } else {
-    codeUrl = code.html()
-    codeArray.push(code.html())
-  }
-
-
-  code.replaceWith('<a class="movie_pn">' + code.html() + '</a>')
+  code.html(code.text().replace("_", "-"));
+  code.html(code.text().toUpperCase());
   code = $(this).find(".movie_pn");
-  code.attr(`id`, `${x}`).attr
-  code.after(`<a class="fa-regular fa-copy" onclick="copyToClipboard('#${x}')" style="margin-left: 5px;"></a>`)
-  var sukebei = `https://sukebei.nyaa.si/?q=${codeUrl}&s=seeders&o=desc`
-  var missav = `https://missav.com/en/search/${codeUrl}`
-  $(this).find(".loading").before(`<a href='https://img.javstore.net/search/images/?q="${codeUrl}"' target="_blank"><img src="https://img.javstore.net/content/images/system/default/favicon.png" width='17' height='17'></img></a>`)
+  let match = /^(carib|caribpr|1pon|paco)+-(\d.+)/gi;
+  let hasil = match.exec(code.html());
+  match.test(code.html()); // if i didnt add this the fucking if test will not working bruh...
+  var codeUrl;
+  if (match.test(code.html())) {
+    codeUrl = hasil[2];
+    codeArray.push(hasil[2]);
+  } else {
+    codeUrl = code.html();
+    codeArray.push(code.html());
+  }
+
+  code.replaceWith('<a class="movie_pn">' + code.html() + "</a>");
+  code = $(this).find(".movie_pn");
+  code.attr(`id`, `${x}`).attr;
+  code.after(
+    `<a class="fa-regular fa-copy" onclick="copyToClipboard('#${x}')" style="margin-left: 5px;"></a>`
+  );
+  var missav = `https://missav.com/en/search/${codeUrl}`;
+
+  const fc2 = /^(FC2-PPV)+-(\d.+)/i;
+  // console.log(codeUrl, fc2.test(codeUrl))
+  if (fc2.test(codeUrl)) {
+    codeUrl = fc2.exec(codeUrl)[2];
+  }
+  // add javstore button
+  $(this)
+    .find(".loading")
+    .before(
+      `<a href='https://img.javstore.net/search/images/?q="${codeUrl}"' target="_blank"><img src="https://img.javstore.net/content/images/system/default/favicon.png" width='17' height='17'></img></a>`
+    );
 
   GM_xmlhttpRequest({
     method: "GET",
     url: missav,
     onload: (response) => {
-      let grid = $(response.responseText).find('div.grid div.relative');
+      let grid = $(response.responseText).find("div.grid div.relative");
       if (grid.length) {
-        code.attr(`href`, missav).attr(`target`, "_blank");
-        code.css("color", "#fe628e").css("font-size", "large").css("font-weight", "600").html(code.html().toUpperCase());
-        $(this).find("div.movie_image").removeClass("unsubscribe_image")
+        code
+          .css("color", "#fe628e")
+          .css("font-size", "large")
+          .css("font-weight", "600")
+          .attr(`href`, missav)
+          .attr(`target`, "_blank");
+        $(this).find("div.movie_image").removeClass("unsubscribe_image");
       } else {
-        $(this).find("div.movie_image").addClass("unsubscribe_image")
+        $(this).find("div.movie_image").addClass("unsubscribe_image");
       }
-    }
+    },
   });
-  x++
+  x++;
 });
-
 
 function getSukebei(codeArray) {
   let codes = "";
@@ -95,9 +116,10 @@ function getSukebei(codeArray) {
     url: sukebeis,
     onload: (response) => {
       codeArray.forEach((code) => {
-        const contain = $(`.movie_pn:contains('${code}')`).parent();
-        const loading = contain.find(".loading");
-        const movie_img = contain.parent().find("div.movie_image");
+        const movie_info = $(`div.movie_info:contains('${code}')`);
+        const loading = movie_info.find(".loading");
+        const movie_img = movie_info.find("div.movie_image");
+
         const containCode = $(response.responseText).find(
           `td:contains("${code}")`
         );
@@ -130,14 +152,12 @@ function getSukebei(codeArray) {
           <a class="fa fa-fw fa-download" href='${torrent}'></a>
           <a class="fa fa-fw fa-magnet" href='${magnet}'></a>
           <a style='color: lawngreen; font-size: large; font-weight: 600;' >${seedsNumber}</a>
-        `);
+          `);
           movie_img.removeClass("unsubscribe_image");
-          // let contain = $(`.movie_pn:contains('${code}')`).parent().find(".loading").html()
-          // console.log(contain)
 
-          console.log(code + " Found :)");
+          console.log(code + " Found on Sukebei :)");
         } else {
-          console.log(code + " Not Found :(");
+          console.log(code + " Not Found on Sukebei :(");
         }
         loading.remove();
       });
@@ -145,5 +165,4 @@ function getSukebei(codeArray) {
   });
 }
 
-$(".movie_pn").after(`<div class='sukebei'><a class='loading'>Sukebei Loading...</a></div>`)
-getSukebei(codeArray)
+getSukebei(codeArray);
